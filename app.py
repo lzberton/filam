d7f538d228187d4b56fcc8272468ad02f4ebf406
Leonardo Berton <leozberton@gmail.com>
2025-07-08 10:00:42 -0300

    UpdateAvisos

--- app.py
+++ app.py
@@ -522,7 +522,7 @@ nomes = [
     "AVISOS",
 ]
 
-slideshow_images = ["ACERTO.png"]
+slideshow_images = ["ACERTO.png","Aviso2.png","Aviso3.png"]
 slideshow_duration = len(slideshow_images)
 avisos_index = len(nomes) - 1
 count = st_autorefresh(interval=30000, limit=None, key="auto_refresh")
