3a1ad4012a309de16c63a6f263c9f0123f93b45c
Leonardo Berton <leozberton@gmail.com>
2025-09-10 08:03:21 -0300

    Update Aviso

--- app.py
+++ app.py
@@ -517,7 +517,7 @@ nomes = [
     "AVISOS",
 ]
 
-slideshow_images = ["AvisoAgo.png", "Aviso2.png", "Aviso3.png"]
+slideshow_images = ["Comunicados.jpeg", "Aviso2.png", "Aviso3.png"]
 slideshow_duration = len(slideshow_images)
 avisos_index = len(nomes) - 1
 
